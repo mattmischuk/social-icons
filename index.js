4e81090b66a13ef37597a6b0ee490ece8736f2fe
Matt Mischuk <m.mischuk@gmail.com>
2018-01-19 15:46:41 -0500

    Initial Commit 👋

---
+++ index.js
@@ -0,0 +1,23 @@
+export const SOCIAL_ICON_SVG = {
+  FACEBOOK: './assets/facebook/icon_facebook.svg',
+  LINKEDIN: './assets/linkedin/icon_linkedin.svg',
+  TWITTER: './assets/twitter/icon_twitter.svg',
+};
+
+export const SOCIAL_ICON_PNG = {
+  FACEBOOK: {
+    AT_1X: './assets/facebook/icon_facebook@1x.png',
+    AT_2X: './assets/facebook/icon_facebook@2x.png',
+    AT_3X: './assets/facebook/icon_facebook@3x.png',
+  },
+  LINKEDIN: {
+    AT_1X: './assets/linkedin/icon_linkedin@1x.png',
+    AT_2X: './assets/linkedin/icon_linkedin@2x.png',
+    AT_3X: './assets/linkedin/icon_linkedin@3x.png',
+  },
+  TWITTER: {
+    AT_1X: './assets/twitter/icon_twitter@1x.png',
+    AT_2X: './assets/twitter/icon_twitter@2x.png',
+    AT_3X: './assets/twitter/icon_twitter@3x.png',
+  },
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Social Icons
+
+This is a dead simple collection of up-to-date social icons. There's only a few in here at the moment. Make an issue to request a one that is missing.
+
+### Icons Included
+
+- Facebook
+- LinkedIn
+- Twitter
